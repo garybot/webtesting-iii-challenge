2713e6b56c420d54b955a3fe21da9f507ac42f88
garybot <argot.pryer@gmail.com>
2019-12-04 20:34:42 -0800

    Wrote all of the tests for Display and Gate.

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,19 @@
-// Test away
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+
+// Dashboard
+//
+// shows the controls and display
+//
+
+test("shows the controls and display", () => {
+
+})
+
+test("defaults to `unlocked` and `open`", () => {
+  const wrapper = rtl.render(<Dashboard />);
+  expect(wrapper.getByText(/unlocked/i));
+  expect(wrapper.getByText(/open/i));
+})
